b498adeb12fefb1f68059641daab91128a78a41c
Jonas Harms, CFA <jonas@harms360.com>
2025-11-05 14:32:21 +0100

    Create place_stamp.js

---
+++ place_stamp.js
@@ -0,0 +1,11 @@
+var p = 0; 
+var Xstart = 515, Ystart = 810, Xend = 780;
+var rect = [ Xstart, Ystart, Xend ];
+
+this.addAnnot({
+    page:      p,
+    type:      "Stamp",        
+    name:      "Reviewed",     
+    rect:      rect,
+
+});
